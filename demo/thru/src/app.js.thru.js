bc9edc95667b8392b3e5018914185bb9a0c2038e
barcek <barcek@gmx.com>
2021-04-25 10:06:27 +0100

    style: improved error messages, adding utility functions, & renamed existing utility function

--- demo/thru/src/app.js.thru.js
+++ demo/thru/src/app.js.thru.js
@@ -2,7 +2,17 @@
     Utilities
 */
 
-const renamePkgToVar = name => name.replace(/-(\w)/g, (hyphen, char) => char.toUpperCase());
+const namePkgVar = name => name.replace(/-(\w)/g, (hyphen, char) => char.toUpperCase());
+
+const sIfMultiple = arr => arr.length > 1 ? 's' : '';
+
+const quoteItems = arr => arr.map(item => `'${item}'`);
+
+const createList = arr => arr.reduce((acc, item, i, arr) =>
+        i === 0            ? item :
+        i < arr.length - 1 ? acc + ', ' + item
+                           : acc + ' & ' + item
+, '');
 
 /*
     Resolvers
@@ -37,7 +47,7 @@ const resolvers = {
 
         modules = modules.map(module => {
             if (moduleUses[module.use] && !moduleUses[module.use][module.src]) {
-                throw new Error(`Source '${module.src}' unsupported for use '${module.use}'.`);
+                throw new Error(`Source '${module.src}' unsupported for use '${module.use}'`);
             };
             if (moduleUses[module.use] && moduleUses[module.use][module.src]) {
                 for (let key of Object.keys(moduleUses[module.use][module.src])) {
@@ -64,7 +74,7 @@ const resolvers = {
             };
         };
         if (varsAbsent.length > 0) {
-            throw new Error(`Variable(s) ${varsAbsent.join(', ')} required.`);
+            throw new Error(`Variable${sIfMultiple(varsAbsent)} ${createList(quoteItems(varsAbsent))} required`);
         };
 
         return {
@@ -88,7 +98,7 @@ const resolvers = {
         if (comp.modules) {
             const furtherLines = comp.modules.reduce((acc, module) => {
                 const src = module.src;
-                src !== 'express' && acc.push(`import ${renamePkgToVar(src)} from '${src}';`);
+                src !== 'express' && acc.push(`import ${namePkgVar(src)} from '${src}';`);
                 return acc;
             }, []);
             contentItems.push(...furtherLines);
@@ -159,7 +169,7 @@ const resolvers = {
                 module.mid
                     && (typeof module.mid === 'string'
                         ? acc.push(module.mid)
-                        : acc.push(`app.use(${renamePkgToVar(module.src)}());`));
+                        : acc.push(`app.use(${namePkgVar(module.src)}());`));
                 return acc;
             }, []);
             contentItems.push(...furtherLines);
